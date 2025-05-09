122f6ac03841433f651660a2199b12be4a0a75ec
blf <694392968@qq.com>
2025-05-09 17:28:42 +0800

    第三次

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+// 创建Vue应用实例
+const app = createApp(App);
+
+// 挂载到HTML中的<div id="app">
+app.mount('#app');
